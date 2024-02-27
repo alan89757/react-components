bcad7f64fd28a14886af52335e28ffc4b205c405
liangrongjun <17612176307@wangxiao.cn>
2024-02-27 18:17:11 +0800

    更新storybook，有报错

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
+  framework: { name: '@storybook/react-webpack5', options: { fastRefresh: false } },
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
